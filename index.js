1bcdd5b9fdc6022fb1a364833f9cf0834d79a519
nazero3 <nazerrabah1994@gmail.com>
2023-04-04 21:05:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code here
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink;
+    }
+
+}
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+
+}
+
+class Dinner {
+    #dissert
+    constructor(salad, soup, entree, dissert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.#dissert = dissert;
+    }
+}
